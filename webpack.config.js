0cfb1981a05e981e4f135e1e3e014102c93cc3ea
Lee Smith <leesmith0078@gmail.com>
2021-05-31 15:12:10 +0100

    Setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const { webpack } = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if(process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development' });
 }
-// process.env.NODE_ENV
 
 module.exports = (env) => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
-  
+
   return {
     entry: './src/app.js',
     output: {
